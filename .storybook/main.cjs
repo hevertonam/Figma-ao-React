00f1d3d9310d6ca309abe1a144ea2b1da8b071cf
Heverton <hevertonam2018@gmail.com>
2022-10-16 13:14:32 -0300

    alter name project main.cjs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -18,7 +18,7 @@ module.exports = {
   staticDirs: ['../public'],
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/ignite-lab-design-system/'
+      config.base = '/Figma-ao-React/'
     }
     return config
   }
