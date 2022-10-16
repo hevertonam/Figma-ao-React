385585252d667ac8efe9325ea132abf13e29b4ca
Heverton <hevertonam2018@gmail.com>
2022-10-16 11:42:28 -0300

    ci: Add publish storybook worklow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, {configtype}) =>{
+    if (configtype === 'PRODUTION'){
+      config.base = '/Figma-ao-React/'
+    }
+
+    return config
   }
 }
